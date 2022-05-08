3a35fe6cdc18bc6f29e7fc50a90a9b7f674c7100
dolivare5 <deiverolivares2@gmail.com>
2022-05-08 11:31:13 -0500

    Cambie la región a español colombia y con ello el idioma de las fechas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
@@ -6,6 +6,10 @@ import {SharedModule} from "./shared/shared.module";
 import {AppRouterModule} from "./app-router.module";
 import { VentasModule } from './ventas/ventas.module';
 
+// Cambiar el locale de la app
+import localeEsp from '@angular/common/locales/es-CO';
+import {registerLocaleData} from "@angular/common";
+registerLocaleData(localeEsp);
 
 @NgModule({
     declarations: [
@@ -17,7 +21,9 @@ import { VentasModule } from './ventas/ventas.module';
         SharedModule,
         VentasModule
     ],
-    providers: [],
+    providers: [
+        {provide: LOCALE_ID, useValue: 'es-CO'}
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
