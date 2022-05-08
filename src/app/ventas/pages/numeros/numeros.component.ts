97e9d5632b728acfbe83003946b048029f26ee1f
dolivare5 <deiverolivares2@gmail.com>
2022-05-08 12:43:59 -0500

    Aprendí a utilizar Pipes de tipo number para formatear dinero, monedas y porcentajes.

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,16 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-numeros',
-  templateUrl: './numeros.component.html',
-  styles: [
-  ]
+    selector: 'app-numeros',
+    templateUrl: './numeros.component.html',
+    styles: [
+    ]
 })
-export class NumerosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NumerosComponent {
+    ventasNetas : number = 3580998.999;
+    porcentaje : number = 0.48;
 }
