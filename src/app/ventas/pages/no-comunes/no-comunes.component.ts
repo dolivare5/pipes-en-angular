957faa3f9a33ac1d9236848b127f6bb9df189d47
dolivare5 <deiverolivares2@gmail.com>
2022-05-09 14:12:58 -0500

    Comprendí el funcionamiento de i18nSelect y el i18nPluralPipe.

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -7,13 +7,31 @@ import { Component } from '@angular/core';
     ]
 })
 export class NoComunesComponent {
+    // i18nSelect
     nombre: string = 'Susana';
     genero: string = 'femenino';
     
     invitacionMapa =  {
         masmulino: 'invitarlo',
         femenino: 'invitarla'
-}
+    }
+    
+    // i18nPlural
+    cliente: string[] = ['Maria', 'Pedro', 'Juan'];
+    clientesMapa = {
+        '=0' : 'no tenemos ningún cliente esperando',
+        '=1' : 'tenemos un cliente esperando',
+        'other' : 'tenemos # clientes esperando'
+    }
+    
+    cambiarCliente(): void {
+        this.nombre = 'Melisa';
+        this.genero = 'femenino';
+    }
+    
+    borrarCliente(): void {
+        this.cliente.pop();
+    }
 
 
 }
