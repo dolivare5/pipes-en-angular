f5fe523084ae9ecf9eee9dbf3c219ca65034c956
dolivare5 <deiverolivares2@gmail.com>
2022-05-17 18:04:13 -0500

    Comprendí el funcionamiento del KeyValuePipe, JsonPipe y Async Pipe.

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {interval} from "rxjs";
 
 @Component({
     selector: 'app-no-comunes',
@@ -33,5 +34,27 @@ export class NoComunesComponent {
         this.cliente.pop();
     }
 
+    // KeyValue Pipe
+    persona = {
+        nombre: 'Fernando',
+        edad: 35,
+        direccion: 'Barranquilla, Colombia'
+    }
+    
+    // Json Pipe
+    heroes =[
+        {nombre: 'Superman', vuela: true},
+        {nombre: 'Robin', vuela: false},
+        {nombre: 'Aqua-man', vuela: false},
+    ]
+    
+    // Async Pipe
+    miObservable= interval(1000);
+    
+    valorPromesa = new Promise((resolve, reject) => {
+        setTimeout(() =>{
+            resolve('Fin de la promesa, Tenemos data de promesa')
+        }, 3500)
+    })
 
 }
